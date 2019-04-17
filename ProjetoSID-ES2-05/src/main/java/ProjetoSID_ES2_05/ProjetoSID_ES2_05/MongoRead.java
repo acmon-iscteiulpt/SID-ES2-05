cc351a53751adacde015786861d6d5b9c0ef97c2
jcagz-iscteiul <jcagz@iscte-iul.pt>
2019-04-17 19:53:03 +0100

    Adicionado mostraMongodb

--- ProjetoSID-ES2-05/src/main/java/ProjetoSID_ES2_05/ProjetoSID_ES2_05/MongoRead.java
+++ ProjetoSID-ES2-05/src/main/java/ProjetoSID_ES2_05/ProjetoSID_ES2_05/MongoRead.java
@@ -1,5 +1,41 @@
 package ProjetoSID_ES2_05.ProjetoSID_ES2_05;
 
-public class MongoRead {
+
+import com.mongodb.*;
+
+
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.*;
+
+import org.json.JSONObject;
+
+
+
+public class MongoRead  {
+
+	private static final String bd = "Dados_sensoresBD";
+	private static final String collection = "dados_luminosidade";
+
+
+	public void mostraMongoDB() {
+		MongoClient mongoClient1 = new MongoClient();
+
+		DB db = mongoClient1.getDB(bd);
+		DBCollection table = db.getCollection(collection);
+		DBCursor cursor = table.find();
+		while(cursor.hasNext()) {
+
+			System.out.println(cursor.next());
+		}
+	}
+
+
+
+
+
+
 
 }
