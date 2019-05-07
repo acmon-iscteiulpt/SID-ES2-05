21754c84aee74ed25471c03b943b8bab3e2c780b
acmon-iscteiulpt <43675261+acmon-iscteiulpt@users.noreply.github.com>
2019-05-07 10:06:18 +0100

    Subscriber descarta valores

--- ProjetoSID-ES2-05/src/main/java/ProjetoSID_ES2_05/ProjetoSID_ES2_05/Subscriber.java
+++ ProjetoSID-ES2-05/src/main/java/ProjetoSID_ES2_05/ProjetoSID_ES2_05/Subscriber.java
@@ -12,9 +12,21 @@ public class Subscriber extends Cliente {
 
 	private static final String bd = "bd_dados_sensores";
 	private static final String collection = "collection_dados_sensores";
+	private static final int discrepanciaTemperatura = 10;
+	private static final int discrepanciaLuminosidade = 10;
 	
 	private MongoWrite mw;
 	private MongoRead mr;
+	private boolean primeiroDadoTemperaturaRecebido = false;
+	private boolean primeiroDadoLuminosidadeRecebido = false;
+	private double lastResultadoLuminosidade;
+	private double lastResultadoTemperatura;
+	private int limiteSuperiorLuminosidade;
+	private int limiteInferiorLuminosidade;
+	private int limiteSuperiorTemperatura;
+	private int limiteInferiorTemperatura;
+	private double percentagemLuminosidade;
+	private double percentagemTemperatura;
 
 	public Subscriber(String clientID) {
 		super(clientID);
@@ -63,26 +75,148 @@ public class Subscriber extends Cliente {
 		if (jsonObj.has("tmp") && jsonObj.has("cell")) {
 			System.out.println("Inserindo na mongoDB");
 
+			if(!descartarValorTemperatura(mp.getTmp())) {
+				mw.write("temperatura", mp.getTmp(), mp.getDat(), mp.getTim());
+			}
+			if(!descartarValorLuminosidade(mp.getCell())) {
+				mw.write("luminosidade", mp.getCell(), mp.getDat(), mp.getTim());
+			}
 			
-			mw.write("temperatura", mp.getTmp(), mp.getDat(), mp.getTim());
-			mw.write("luminosidade", mp.getCell(), mp.getDat(), mp.getTim());
 		}
 		if (jsonObj.has("tmp") && !jsonObj.has("cell")) {
 			System.out.println("tem tudo menos a cell(luminosidade)");
 			System.out.println("Inserindo na mongoDB dados apenas da temperatura na bd");
 
 
+			if(!descartarValorTemperatura(mp.getTmp())) {
+				mw.write("temperatura", mp.getTmp(), mp.getDat(), mp.getTim());
+			}
 
-			mw.write("temperatura", mp.getTmp(), mp.getDat(), mp.getTim());
 
 		}
 		if (!jsonObj.has("tmp") && jsonObj.has("cell")) {
 			System.out.println("tem tudo menos a tmp(temperatura)");
 			System.out.println("Inserindo na mongoDB dados apenas da luminosidade na bd");
-
-			mw.write("luminosidade", mp.getCell(), mp.getDat(), mp.getTim());
+			
+			if(!descartarValorLuminosidade(mp.getCell())) {
+				mw.write("luminosidade", mp.getCell(), mp.getDat(), mp.getTim());
+			}
+			
 
 		}
+		
+	}
+	
+	/*
+	 * Método que retorna um boolean a indicar se descarta o valor ou não
+	 * 	--> Só descarta o valor quando há modificação no atributo lastResultadoTemperatura;
+	 */
+	public boolean descartarValorTemperatura(double valorTemperatura) {
+		boolean temperaturaValida;
+		boolean descarta = false;;
+		//verificar se o primeiro resultado recebido está dentro dos limites se sim --> primeiroResultadoRecebido true
+		if(!primeiroDadoTemperaturaRecebido) {
+			System.out.println("Subscriber já recebeu o primeiro valor de Temperatura do sensor");
+			temperaturaValida = valorDentroDosLimites_Temperatura(valorTemperatura);
+			lastResultadoTemperatura = (temperaturaValida) ? valorTemperatura: 0;
+			descarta = (temperaturaValida) ? false: true;
+			primeiroDadoTemperaturaRecebido = (temperaturaValida) ? true: false;
+			System.out.println("Descarta valor: " + descarta);
+			return descarta;
+		} else {
+			//comparar o valor do lastResultado com o valor atual do sensor
+			double difValorTemperatura = lastResultadoTemperatura - valorTemperatura;
+			if(difValorTemperatura>=-discrepanciaTemperatura && difValorTemperatura<=discrepanciaTemperatura) {
+				temperaturaValida = valorDentroDosLimites_Temperatura(valorTemperatura);
+				lastResultadoTemperatura = (temperaturaValida) ? valorTemperatura: lastResultadoTemperatura;
+				descarta = (temperaturaValida) ? false: true;
+				System.out.println("Descarta valor: " + descarta);
+				return descarta;
+			} else {
+				System.out.println("Descarta valor: " + true + " discrepância muito elevada!");
+				return true;
+			}
+			
+		}
+	}
+	
+	public boolean descartarValorLuminosidade(double valorLuminosidade) {
+		boolean luminosidadeValida;
+		boolean descarta = false;
+		if(!primeiroDadoLuminosidadeRecebido) {
+			System.out.println("Subscriber já recebeu o primeiro valor de Luminosidade do sensor");
+			luminosidadeValida = valorDentroDosLimites_Luminosidade(valorLuminosidade);
+			lastResultadoLuminosidade = (luminosidadeValida) ? valorLuminosidade: 0;
+			primeiroDadoLuminosidadeRecebido = (luminosidadeValida) ? true: false;
+			descarta = (luminosidadeValida) ? false: true;
+			System.out.println("Descarta o valor: " + descarta);
+			return descarta;
+		} else {
+			double difValorLuminosidade = lastResultadoLuminosidade - valorLuminosidade;
+			if(difValorLuminosidade>=-discrepanciaLuminosidade && difValorLuminosidade<=discrepanciaLuminosidade) {
+				luminosidadeValida = valorDentroDosLimites_Luminosidade(valorLuminosidade);
+				lastResultadoLuminosidade = (luminosidadeValida) ? valorLuminosidade: lastResultadoLuminosidade;
+				descarta = (luminosidadeValida) ? false: true;
+				System.out.println("Descarta valor: " + descarta);
+				return descarta;
+			} else {
+				//Existe uma discrepância muito elevada e por isso descarta-se o valor
+				System.out.println("Descarta valor: " + true + " discrepância muito elevada!");
+				return true;
+			}
+		}
+	}
+	
+	/*
+	 * Se o valor da temperatura for igual ao limiarSuperior ou ao limiarInferior --> envia notificação
+	 */
+	public boolean valorDentroDosLimites_Temperatura(double valorTemperatura) {
+		int difLimites = limiteSuperiorTemperatura - limiteInferiorTemperatura;
+		double difPercentagem = 100 - percentagemTemperatura;
+		double limiarSuperior = limiteInferiorTemperatura + (difLimites * (percentagemTemperatura/100));
+		double limiarInferior = limiteInferiorTemperatura + (difLimites * (difPercentagem/100));
+		System.out.println("LimiarSuperior: " + limiarSuperior);
+		System.out.println("LimiarInferior: " + limiarInferior);
+		if(valorTemperatura >= limiteSuperiorTemperatura || valorTemperatura <= limiteInferiorTemperatura) {
+			enviarNotificação();
+			System.out.println("Valor Temperatura: " + valorTemperatura + " ultrapassou os limites");
+			return false;
+		}
+		if(valorTemperatura >= limiarSuperior || valorTemperatura <= limiarInferior) {
+			enviarNotificação();
+			System.out.println("Valor Temperatura: " + valorTemperatura + " ultrapassou os limiares mas está dentro dos limites");
+			return true;
+		}
+		System.out.println("Valor Temperatura: " + valorTemperatura + " está dentro dos limiares");
+		return true;
+	}
+	
+	/*
+	 * Se o valor da luminosidade for igual ao limiarSuperior ou ao limiarInferior --> envia notificação
+	 */
+	public boolean valorDentroDosLimites_Luminosidade(double valorLuminosidade) {
+		int difLimites = limiteSuperiorLuminosidade - limiteInferiorLuminosidade;
+		double difPercentagem = 100 - percentagemLuminosidade;
+		double limiarSuperior = limiteInferiorLuminosidade + (difLimites * (percentagemLuminosidade/100));
+		double limiarInferior = limiteInferiorLuminosidade + (difLimites * (difPercentagem/100));
+		System.out.println("Limiar Superior: " + limiarSuperior);
+		System.out.println("Limiar Inferior: " + limiarInferior);
+		if(valorLuminosidade >= limiteSuperiorLuminosidade || valorLuminosidade <= limiteInferiorLuminosidade) {
+			enviarNotificação();
+			System.out.println("Valor Temperatura: " + valorLuminosidade + " ultrapassou os limites");
+			return false;
+		}
+		if(valorLuminosidade >= limiarSuperior || valorLuminosidade <= limiarInferior) {
+			enviarNotificação();
+			System.out.println("Valor Luminosidade: " + valorLuminosidade + " ultrapassou os limiares mas está dentro dos limites");
+			return true;
+		}
+		System.out.println("Valor Luminosidade: " + valorLuminosidade + " está dentro dos limiares");
+		return true;
+	}
+	
+	//Falta definir
+	public void enviarNotificação() {
 		
 	}
 
